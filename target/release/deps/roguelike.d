398ade249c7d700a82e55325408cb12d745c7e38
Teo Oliver <teo.liz.oli@gmail.com>
2020-11-14 21:31:51 +0000

    delete target folder

--- target/release/deps/roguelike.d
+++
@@ -1,5 +0,0 @@
-/Users/teoliver/Documents/Rust_Projects/roguelike/target/release/deps/roguelike: src/main.rs
-
-/Users/teoliver/Documents/Rust_Projects/roguelike/target/release/deps/roguelike.d: src/main.rs
-
-src/main.rs:
--- target/release/roguelike.d
+++
@@ -1,1 +0,0 @@
-/Users/teoliver/Documents/Rust_Projects/roguelike/target/release/roguelike: /Users/teoliver/Documents/Rust_Projects/roguelike/src/main.rs
